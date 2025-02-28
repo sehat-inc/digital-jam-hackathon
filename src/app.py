1fb762b7440f784182eff8dc635e7ccdae0e46eb
maazhamidexe <u2022655@giki.edu.pk>
2025-03-01 00:35:25 +0500

    gunicorn2.0

--- src/app.py
+++ src/app.py
@@ -320,4 +320,5 @@ def download_contract(contract_id):
             os.unlink(temp_file.name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
